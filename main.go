5a641b19e0ef26ebffb962d9603b250d7ceaa8a9
eau <eau+code@unix4fun.net>
2019-12-21 22:34:59 +0100

    added flags

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 //go:generate protoc oidc.proto -I. -I$HOME/tools/pb3/include -I/home/rival/dev/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.1/third_party/googleapis --grpc-gateway_out=logtostderr=true:.
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/ermites-io/oidc"
@@ -21,43 +22,35 @@ const (
 //
 // we will listen on 2 ports
 // 8888: GRPC service
-//
-// 80: REST gateway
-
-//
-// ok OIDC services NEEDS to make this discovery.
-//
-
-/*
-type jwkEnv struct {
-	keys []string `json:"keys"`
-}
-*/
+// 8000: REST gateway
 
 func main() {
-	fmt.Printf("oauth dev service\n")
+	fmt.Printf("Openid Grpc Rp Example service\n")
+	fmt.Printf("this is an implementation example using GRPC & GRPC-gateway of an OpenID authentication party\n")
 
-	google, err := oidc.NewProvider("google", "./google-openid-configuration")
+	config := flag.String("config", "openid-configuration", "openid configuration")
+	name := flag.String("name", "", "provider name")
+	clientIdFlag := flag.String("id", "", "client id")
+	clientSecretFlag := flag.String("secret", "", "client secret")
+	clientRedirectFlag := flag.String("redirect", "", "redirect url configured")
+
+	flag.Parse()
+	//argv := flag.Args()
+
+	idp, err := oidc.NewProvider(*name, *config)
 	if err != nil {
 		panic(err)
 	}
+
 	// setup the new provider..
-	err = google.SetAuth("118314873199clientid.idp.com", "clientsecret2341321421", "https://login.ermite.io/oauth")
+	err = idp.SetAuth(*clientIdFlag, *clientSecretFlag, *clientRedirectFlag)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("GOOGLE Provider: %v\n", google)
-
-	/*
-		microsoft, err := NewProvider("microsoft", "./microsoft-openid-configuration")
-
-		// setup the new provider..
-		microsoft.SetClient("118314873199.microsoftblablacontent.com", "dkjsaldasjlkjwc39939re1", "https://login.ermite.io/oauth")
-		fmt.Printf("MICROSOFT Provider: %v\n", google)
-	*/
+	fmt.Printf("Provider: %v\n", idp.GetName())
 
 	// add providers
-	svc := NewOidcService(google)
+	svc := NewOidcService(idp)
 	svc.SetFailUrl("https://login.ermite.io/login-failed")
 	svc.SetOkUrl("https://login.ermite.io/login-ok")
 
@@ -66,27 +59,5 @@ func main() {
 	go grpcServer(svc, grpcConnect)
 
 	// then rest
-	//restServer(restService, &oaf)
 	restServer(svc, restConnect)
-
-	// STATE CREATION / VERIFICATION TPW
-	//func State(password, secret []byte, oidcNonce string) (*OidcState, error) {
-	//oidcNonce := uuid.New().String()
-
-	//
-	/*
-		parseOidcConf("./google-openid-configuration")
-		parseOidcConf("./microsoft-openid-configuration")
-	*/
-
-	// password and secret can be secret as they only need to be consistent
-	// across a set of oidc logging systems
-	// so it needs to be a parameter.
-	//
-	//
-	// i would need something like registerOidc("endpoint", "openid configuration")
-	// :
-	// oidc.NewProvider("google", "google-openid-conf")
-	// oidc.NewProvider("microsoft", "microsoft-openid-conf")
-	// oidc.NewProvider("github", "ping-openid-conf")
 }
